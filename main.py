28905f4e8692383953cb8d0a9ad724183cfd242b
Dosoy <olaf.meister@mail.ru>
2021-10-12 16:51:03 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,39 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+if __name__ == '__main__':
+    name = "Dosoy"
+    digit = 12
+    list_of_digits = [1, 2, 3, 4, 5]
+    str_list = str(list_of_digits)
+    print(str_list)
+    print(type(str_list))
+    print(list_of_digits)
+    print(type(list_of_digits))
+
+    user_id = 234
+    print(user_id)
+    print(type(user_id))
+
+    user_id2 = 5.4
+    print(user_id2)
+    print(type(user_id2))
+
+    a = (5, 7, 8, 9)
+    print(a)
+    print(type(a))
+    animal = {'cat': 'кошка', 'dog': 'собака', 'cow': 'корова'}
+    print(animal)
+    print(type(animal))
+
+if __name__ == '__main__':
+    c1 = 2, "2"
+    print(c1)
+if __name__ == '__main__':
+    c2 = 3, "3"
+    print(c2)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
